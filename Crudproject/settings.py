148137ead7accc656fb3dcfe3d6af0acb7fbee56
Kiran <kirannarwade50@gmail.com>
2023-01-26 00:03:24 +0530

    added manually

--- Crudproject/settings.py
+++ Crudproject/settings.py
@@ -130,3 +130,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Added Manually
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
